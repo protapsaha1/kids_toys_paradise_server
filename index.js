df5e1cef944b0b4eae0709bfd080065f8b3326fa
protapsaha1 <webprotap09@gmail.com>
2023-05-21 12:46:04 +0600

    set jwt verify

--- index.js
+++ index.js
@@ -25,71 +25,128 @@ const client = new MongoClient(uri, {
     }
 });
 
+
+const verifyUT = (req, res, next) => {
+    // console.log('hitting middle')
+    const authorization = req.headers.authorization;
+    if (!authorization) {
+        return res.status(401).send({ error: true, message: 'unauthorized access' })
+    }
+    const token = authorization.split(' ')[1];
+    jwt.verify(token, process.env.ACCESS_AUTHOR_SECRET_TOKEN, (error, decoded) => {
+        if (error) {
+            return res.status(403).send({ error: true, message: 'unauthorized access' })
+        }
+        req.decoded = decoded;
+        next();
+    })
+}
+
 async function run() {
     try {
         // Connect the client to the server	(optional starting in v4.7)
         await client.connect();
         const toysCollection = client.db("kidsToys").collection("allToys");
+        // JWT 
+        app.post('/jwt', (req, res) => {
+            const user = req.body;
+            const token = jwt.sign(user, process.env.ACCESS_AUTHOR_SECRET_TOKEN, { expiresIn: '1h' });
+            res.send({ token });
+        })
 
-        // .search(search).sort(sort)
-        app.get('/allToys', async (req, res) => {
-            // console.log(req.query.search)
+        // CLIENT SIDE ROUTES
+        // all toy for pagination
+        app.get('/allToys', verifyUT, async (req, res) => {
+            // console.log(req.headers.authorization)
+            const decoded = req.decoded;
+            if (decoded.email !== req.query.email) {
+                return res.status(403).send({ error: 1, message: 'forbidden access' })
+            }
+            console.log('code decoded', decoded)
             const page = parseInt(req.query.page) || 0;
             const limit = parseInt(req.query.limit) || 20;
             const skip = page * limit;
-            // const sort = req.query.sort;
-            const search = {
-                search: { ToyName: 1, category_Name: 1 }
+            let query = {};
+            if (req.query?.email) {
+                query = { email: req.query.email }
             }
-            console.log(search)
-            // const search = req.query.;
-            console.log(search)
-            const result = await toysCollection.find().skip(skip).limit(limit).toArray();
+            const result = await toysCollection.find(query).skip(skip).limit(limit).toArray();
             res.send(result);
-        })
+        });
 
+        // app.get('/allToys', verifyUT, async (req, res) => {
+        //     let query = {};
+        //     if (req.query?.email) {
+        //         query = { email: req.query.email }
+        //     }
+        //     const result = await toysCollection.find(query).toArray();
+        //     console.log(result)
+        //     res.send(result);
+        // })
+
+        // toy search
+        app.get('/allToys/search', async (req, res) => {
+            const searchToy = req.query.name;
+            const filter = {
+                name: { $regex: searchToy.ToyName, $options: 'i' }
+            }
+            console.log(filter)
+            const result = await toysCollection.find(filter).toArray();
+            res.send(result)
+        });
+
+        // toys array length
         app.get('/totalToys', async (req, res) => {
             const result = await toysCollection.estimatedDocumentCount();
-            // console.log({ totalToys: result })
             res.send({ totalToys: result });
-        })
+        });
 
-        app.post('/allToys', async (req, res) => {
-            const getClient = req.body;
-            const result = await toysCollection.insertOne(getClient);
-            res.send(result);
-        })
 
+
+        // single toy find for details
         app.get('/allToys/:id', async (req, res) => {
             const id = req.params.id;
             const query = { _id: new ObjectId(id) };
             const result = await toysCollection.findOne(query);
             res.send(result);
-        })
+        });
 
+        // create toy
+        app.post('/allToys', async (req, res) => {
+            const getClient = req.body;
+            const result = await toysCollection.insertOne(getClient);
+            res.send(result);
+        });
+
+        // delete single toy
         app.delete('/allToys/:id', async (req, res) => {
             const id = req.params.id;
             const query = { _id: new ObjectId(id) };
             const result = await toysCollection.deleteOne(query);
             res.send(result);
-        })
+        });
 
-        app.put('/allToys', async (req, res) => {
+        // update toy
+        app.put('/allToys/:id', async (req, res) => {
             const toyUpdate = req.body;
             const id = req.params.id;
-            const query = { _id: new ObjectId(id) };
+            const filter = { _id: new ObjectId(id) };
             const option = { upsert: true };
             const toyUpdated = {
                 $set: {
-                    name: toyUpdate.ToyName,
-                    price: toyUpdate.ToyPrice,
-                    photo: toyUpdate.ToyPhoto,
-                    seller: toyUpdate.SellerName
+                    ToyPrice: toyUpdate.ToyPrice, ToyQuantity: toyUpdate.ToyQuantity, ToyDetails: toyUpdate.ToyDetails
                 }
             }
-            const result = await toysCollection.updateOne(query, toyUpdated, option);
+            const result = await toysCollection.updateOne(filter, toyUpdated, option);
             res.send(result);
-        })
+        });
+
+
+
+
+
+
+
         // Send a ping to confirm a successful connection
         await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
