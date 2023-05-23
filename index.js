ef6d98081f56c65353e4c86de1f24f617e75fcb9
protapsaha1 <webprotap09@gmail.com>
2023-05-24 01:47:41 +0600

    jwt

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const cors = require("cors");
-const jwt = require("jsonwebtoken");
+// const jwt = require("jsonwebtoken");
 const app = express();
 require("dotenv").config();
 const port = process.env.PORT || 5001;
@@ -12,7 +12,7 @@ app.use(cors());
 app.use(express.json());
 // middleware
 
-
+// console.log(process.env.ACCESS_AUTHOR_SECRET_TOKEN)
 
 const uri = `mongodb+srv://${process.env.DB_PROJECT_TITLE}:${process.env.DB_KEY_PASS}@cluster0.cpjgoyc.mongodb.net/?retryWrites=true&w=majority`;
 
@@ -26,22 +26,25 @@ const client = new MongoClient(uri, {
 });
 
 
-const verifyUT = (req, res, next) => {
-    const authorization = req.headers.authorization;
-    if (!authorization) {
-        return res.status(401).send({ error: true, message: 'unauthorized access' })
-    }
+// const verifyUT = (req, res, next) => {
+//     const authorization = req.headers.authorization;
+//     if (!authorization) {
+//         return res.status(401).send({ error: true, message: 'unauthorized' })
+//     }
+//     console.log('bearer with token', authorization)
 
-    const token = authorization.split(' ')[1];
-    jwt.verify(token, process.env.ACCESS_AUTHOR_SECRET_TOKEN, (error, decoded) => {
-        if (error) {
-            return res.status(403).send({ error: true, message: 'unauthorized access' })
-        }
-        req.decoded = decoded;
-        next();
-    })
+//     const token = authorization.split(' ')[1];
+//     console.log('only token', (token))
+//     jwt.verify(token, process.env.ACCESS_AUTHOR_SECRET_TOKEN, (error, decoded) => {
+//         if (error) {
+//             return res.status(403).send({ error: true, message: 'unauthorized access' })
+//         }
+//         req.decoded = decoded;
+//         console.log('i am authorized', decoded)
+//         next();
+//     })
 
-}
+// }
 
 async function run() {
     try {
@@ -49,34 +52,54 @@ async function run() {
         await client.connect();
         const toysCollection = client.db("kidsToys").collection("allToys");
         // JWT secure
-        app.post('/jwt', (req, res) => {
-            const user = req.body;
-            const token = jwt.sign(user, process.env.ACCESS_AUTHOR_SECRET_TOKEN, { expiresIn: '2h' });
-            res.send({ token });
-        })
+        // app.post('/jwt', (req, res) => {
+        //     const user = req.body;
+        //     const token = jwt.sign(user, process.env.ACCESS_AUTHOR_SECRET_TOKEN, { expiresIn: '2h' });
+        //     console.log('user 57 line', token)
+        //     res.send({ token });
+        // })
 
         // CLIENT SIDE ROUTES
         // all toy for pagination
-        app.get('/allToys', verifyUT, async (req, res) => {
-            const decoded = req.decoded;
-            if (decoded.email !== req.query.email) {
-                return res.status(403).send({ error: 1, message: 'forbidden access' })
-            }
-            else {
-                const page = parseInt(req.query.page) || 0;
-                const limit = parseInt(req.query.limit) || 20;
-                const skip = page * limit;
-                let query = {};
-                if (req.query?.email) {
-                    query = { email: req.query?.email }
-                }
-                const result = await toysCollection.find(query).skip(skip).limit(limit).toArray();
-                return res.status(200).send(result);
-            };
+        app.get('/allToys', async (req, res) => {
+            // console.log('i cought the next function', decoded.email, 'my email', req.query?.email)
+            // if (decoded.email !== req.query.email) {
+            //     return res.status(403).send({ error: 1, message: 'forbidden access' })
+            // }
+            // let query = {};
+            // if (req.query?.email) {
+            //     query = { email: req.query.email }
+            // }
+            // console.log()
+            const page = parseInt(req.query.page) || 0;
+            const limit = parseInt(req.query.limit) || 20;
+            const skip = page * limit;
+            // const decoded = req.decoded;query
+            // console.log('i get my data', decoded.email, 'my email', req.query.email)
+            const result = await toysCollection.find().skip(skip).limit(limit).toArray();
+            console.log('is it my', result)
+            return res.send(result);
         });
+
+        // get data by email 
+        // app.get('/allToys', verifyUT, async (req, res) => {
+        //     const decoded = req.decoded;
+        //     if (decoded.email !== req.query.email) {
+        //         return res.status(403).send({ error: 1, message: 'forbidden access' })
+        //     }
+        //     let query = {};
+        //     if (req.query?.email) {
+        //         query = { email: req.query?.email }
+        //     }
+        //     const result = await toysCollection.find(query).toArray();
+        //     res.send(result)
+        // })
 
         // toys array length
         app.get('/totalToys', async (req, res) => {
+            // if (decoded.email !== req.query.email) {
+            //     return res.status(403).send({ error: 1, message: 'forbidden access' })verifyUT, 
+            // }
             const result = await toysCollection.estimatedDocumentCount();
             res.send({ totalToys: result });
         });
@@ -106,7 +129,7 @@ async function run() {
         });
 
         // update toy
-        app.put('/allToys/:id', async (req, res) => {
+        app.patch('/allToys/:id', async (req, res) => {
             const toyUpdate = req.body;
             const id = req.params.id;
             const filter = { _id: new ObjectId(id) };
@@ -123,33 +146,34 @@ async function run() {
 
         // toy search
         app.get('/allToys', async (req, res) => {
-            const searchToy = req.query.search;
+            const search = req.query.search;
+            console.log(search)
             const filter = {
-                name: { $regex: searchToy, $options: 'i' }
+                title: { $regex: search, $options: 'i' }
             }
-            console.log(filter)
             const result = await toysCollection.find(filter).toArray();
+            console.log(result)
             res.send(result)
         });
 
-        // sort price
-
-        // const option = {
-        //     sort: {
-        //         'price': sort === 'asc' ? 1 : -1
-        //     }
-        // }
-
-
-
-
-
-
+        // // sort price
+        // app.get('/allToys', async (req, res) => {
+        //     const sort = req.query.sort;
+        //     const option = {
+        //         sort: {
+        //             'price': sort === 'asc' ? 1 : -1
+        //         }
+        //     };
+        //     const result = await toysCollection.find(option).toArray();
+        //     res.send(result);
+        // })
 
 
-        app.get('/', (req, res) => {
-            res.send('kids paradise')
-        })
+
+
+
+
+
 
 
 
@@ -164,6 +188,11 @@ async function run() {
 run().catch(console.dir);
 
 
+
+
+app.get('/', (req, res) => {
+    res.send('kids paradise')
+})
 
 
 
